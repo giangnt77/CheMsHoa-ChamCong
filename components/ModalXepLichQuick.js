03fb5f1495249e81387b4ffd59088317f8d800f0
unknown <khoa95905@gmail.com>
2026-08-03 23:12:00 +0700

    style: adjust hour chip options to match store hours (9:00 to 22:00)

--- components/ModalXepLichQuick.js
+++ components/ModalXepLichQuick.js
@@ -238,7 +238,7 @@ export default function ModalXepLichQuick({
                 />
               </div>
               <div className="flex flex-wrap gap-1.5">
-                {['07:00', '08:00', '09:00', '10:00', '11:00', '13:00', '14:00', '17:00', '18:00'].map((t) => (
+                {['09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00'].map((t) => (
                   <button
                     key={t}
                     type="button"
@@ -273,7 +273,7 @@ export default function ModalXepLichQuick({
                 />
               </div>
               <div className="flex flex-wrap gap-1.5">
-                {['12:00', '13:00', '14:00', '17:00', '18:00', '21:00', '22:00', '23:00'].map((t) => (
+                {['13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00'].map((t) => (
                   <button
                     key={t}
                     type="button"
